1f54ea5ac0eed97b8bc5f81f54e82c2714d67b3a
levi hicks <lv.hicks@gmail.com>
2021-09-16 19:44:56 -0500

    install apollo client and connect to Shopify Storefront API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({ 
+  uri: 'https://le-guerno-french-presses.myshopify.com/api/2021-07/graphql.json' 
+});
+
+const middlewareLink = setContext(() => ({
+  headers: {
+    'X-Shopify-Storefront-Access-Token': 'b234cdaadc0b5221ea54e32cd0e82610'
+  }
+}));
+
+const client = new ApolloClient({
+  link: middlewareLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div></div>
+    </ApolloProvider>
   );
 }
 
